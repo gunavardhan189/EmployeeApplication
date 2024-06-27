dea7877a9b0093aba12bca9eaa1153135f52db15
Eeda Gunavardhan <eeda.gunavardhan@globallogic.com>
2024-06-27 11:50:31 +0530

    Secondary Commit

--- src/main/java/com/gl/EmployeeApplication/Controller.java
+++ src/main/java/com/gl/EmployeeApplication/Controller.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Controller {
 
+    //Rest Controller
     @GetMapping(value = "/")
     public ResponseEntity<String> get()
     {
