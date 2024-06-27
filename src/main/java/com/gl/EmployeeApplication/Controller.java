23da6ae1769d0a4bb8132c6e74e8b154cc239559
Eeda Gunavardhan <eeda.gunavardhan@globallogic.com>
2024-06-27 11:44:27 +0530

    Initial Commit

---
+++ src/main/java/com/gl/EmployeeApplication/Controller.java
@@ -0,0 +1,15 @@
+package com.gl.EmployeeApplication;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping(value = "/")
+    public ResponseEntity<String> get()
+    {
+        return ResponseEntity.ok("Employee Details");
+    }
+}
